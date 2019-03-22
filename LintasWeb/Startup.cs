f77f3b5f438032341eaaa91cc2fe5cd12363ae5e
setiawri <ricky.dwsystems@gmail.com>
2019-03-22 10:56:06 +0700

    Add project files.

---
+++ LintasWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LintasMVC.Startup))]
+namespace LintasMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
